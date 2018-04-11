bfa80384a3c189b47a16ae11f6654c91b738c807
Nikolas Schnellbächer <khx0@posteo.net>
2018-04-11 17:05:54 +0200

    manual axis zorder benchmark added

---
+++ mpl_manually_set_axis_zorder/mpl_manually_set_axis_zorder.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2018-04-11
+# file: mpl_manually_set_axis_zorder.py
+##########################################################################################
+
+import time
+import datetime
+import sys
+import os
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib import rc
+from matplotlib.pyplot import legend
+
+mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
+
+def ensure_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+now = datetime.datetime.now()
+now = "%s-%s-%s" %(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RAWDIR = os.path.join(BASEDIR, 'raw')
+OUTDIR = os.path.join(BASEDIR, 'out')
+
+ensure_dir(OUTDIR)
+
+def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac = 0.9):
+    '''
+    True size scaling auxiliary function to setup mpl plots with a desired size.
+    Specify widht and height in cm.
+    lFrac = left fraction   in [0, 1]
+    rFrac = right fraction  in [0, 1]
+    bFrac = bottom fraction in [0, 1]
+    tFrac = top fraction    in [0, 1]
+    returns:
+        fWidth = figure width
+        fHeight = figure height
+    These figure width and height values can then be used to create a figure instance 
+    of the desired size, such that the actual plotting canvas has the specified
+    target width and height, as provided by the input parameters of this function.
+    '''
+    axesWidth = width / 2.54    # convert to inches
+    axesHeight = height / 2.54  # convert to inches
+    fWidth = axesWidth / (rFrac - lFrac)
+    fHeight = axesHeight / (tFrac - bFrac)
+    return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
+
+def Plot(titlestr, X, outname, outdir, pColors,
+         grid = True, savePDF = True, savePNG = False, datestamp = True):
+    
+    mpl.rcParams['xtick.top'] = False
+    mpl.rcParams['xtick.bottom'] = True
+    mpl.rcParams['ytick.right'] = False
+    mpl.rcParams['xtick.direction'] = 'out'
+    mpl.rcParams['ytick.direction'] = 'out'
+    
+    mpl.rc('font',**{'size': 10})
+    mpl.rc('legend',**{'fontsize': 7.5})
+    mpl.rc("axes", linewidth = 0.5)    
+    
+    plt.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Myriad Pro']})
+    plt.rcParams['pdf.fonttype'] = 42  
+    mpl.rcParams['text.usetex'] = False
+    mpl.rcParams['mathtext.fontset'] = 'cm'
+    fontparams = {'text.latex.preamble': [r'\usepackage{cmbright}', r'\usepackage{amsmath}']}
+    mpl.rcParams.update(fontparams)      
+    
+    ######################################################################################
+    # set up figure
+    fWidth, fHeight, lFrac, rFrac, bFrac, tFrac =\
+        getFigureProps(width = 6.5, height = 5.5,
+                       lFrac = 0.20, rFrac = 0.9, bFrac = 0.17, tFrac = 0.95)
+    f, ax1 = plt.subplots(1)
+    f.set_size_inches(fWidth, fHeight)    
+    f.subplots_adjust(left = lFrac, right = rFrac)
+    f.subplots_adjust(bottom = bFrac, top = tFrac)
+    ######################################################################################
+    labelfontsize = 10.0
+    for tick in ax1.xaxis.get_major_ticks():
+        tick.label.set_fontsize(labelfontsize)
+    for tick in ax1.yaxis.get_major_ticks():
+        tick.label.set_fontsize(labelfontsize)
+    
+    ax1.tick_params('both', length = 3.5, width = 0.5, which = 'major', pad = 3.0)
+    ax1.tick_params('both', length = 2.0, width = 0.25, which = 'minor', pad = 3.0)
+    
+    ax1.tick_params(axis='x', which='major', pad = 2.0)
+    ax1.tick_params(axis='y', which='major', pad = 2.0, zorder = 10)
+    ######################################################################################
+    # labeling
+    plt.title(titlestr)
+    ax1.set_xlabel(r'x label', fontsize = 10.0)
+    ax1.set_ylabel(r'y label', fontsize = 10.0)
+    ax1.xaxis.labelpad = 3.0
+    ax1.yaxis.labelpad = 3.0
+    ######################################################################################
+    
+    ax1.plot(X[:, 0], X[:, 1],
+             alpha = 1.0,
+             color = pColors[0],
+             lw = 1.0,
+             label = 'data 1',
+             clip_on = False,
+             zorder = 1)
+
+    ax1.plot(X[:, 0], X[:, 2],
+             alpha = 1.0,
+             color = pColors[1],
+             lw = 1.0,
+             label = 'data 2',
+             clip_on = False,
+             zorder = 11)
+    
+    '''
+    Using the zorder keyword we plot the first line below the
+    axis, whereas we put the second line above it.
+    Comapare the corresponding zorder values:
+    zorder = 1 and 11 for the two lines
+    and zorder = 10 for the axis, as manually set below.
+    The value of zoder = 10 for the axis should be adjusted
+    for your individual needs.
+    '''
+    
+    ###############################################################
+    ###############################################################
+    # manually set the axis zorder here
+    ax1.set_axisbelow(False)
+    for k, spine in ax1.spines.items():  #ax.spines is a dictionary
+        spine.set_zorder(10)
+    ###############################################################
+    ###############################################################
+    
+    ### legend
+    leg = ax1.legend(handlelength = 1.35, 
+                     scatterpoints = 1,
+                     markerscale = 1.0,
+                     ncol = 1)
+    leg.draw_frame(False)
+        
+    ######################################################################################
+    # set plot range and scale
+    ax1.set_xlim(-0.05, 1.05) 
+    ######################################################################################
+    # grid options
+    if (grid):
+        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major', linewidth = 0.4)
+        ax1.grid('on')
+        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor', linewidth = 0.2)
+        ax1.grid('on', which = 'minor')
+    ######################################################################################
+    # save to file
+    if (datestamp):
+        outname += '_' + now
+    if (savePDF): # save to file using pdf backend
+        f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True);
+    if (savePNG):
+        f.savefig(os.path.join(outdir, outname) + '.png', dpi = 600, transparent = False);
+    ######################################################################################
+    # close handles
+    plt.clf()
+    plt.close()
+    return None
+
+if __name__ == '__main__':
+
+    # create data
+    nVisPoints = 500
+    
+    xVals = np.linspace(-0.5, 1.5, nVisPoints)
+    yVals1 = np.array([np.sin(x) for x in xVals])
+    yVals2 = np.array([np.sin(2.0 * x) - 0.1 for x in xVals])
+    X = np.zeros((nVisPoints, 3))
+    X[:, 0] = xVals
+    X[:, 1] = yVals1
+    X[:, 2] = yVals2
+    
+    # plot data                    
+    Plot(titlestr = '',
+         X = X, 
+         outname = 'mpl_manually_set_axis_zorder',
+         outdir = OUTDIR, 
+         pColors = ['C0', 'C1'],
+         grid = False)
+         
---
+++ mpl_manually_set_axis_zorder/mpl_manually_set_axis_zorder_minimal.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2018-04-11
+# file: mpl_manually_set_axis_zorder_minimal.py
+##########################################################################################
+
+import time
+import datetime
+import sys
+import os
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib import rc
+from matplotlib.pyplot import legend
+
+mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
+
+def ensure_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+now = datetime.datetime.now()
+now = "%s-%s-%s" %(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+OUTDIR = os.path.join(BASEDIR, 'out')
+
+ensure_dir(OUTDIR)
+
+if __name__ == '__main__':
+
+    outname = 'mpl_manually_set_axis_zorder_minimal'
+    # create data
+    nVisPoints = 500
+    
+    xVals = np.linspace(-0.5, 1.5, nVisPoints)
+    yVals1 = np.array([np.sin(x) for x in xVals])
+    yVals2 = np.array([np.sin(2.0 * x) - 0.1 for x in xVals])
+    X = np.zeros((nVisPoints, 3))
+    X[:, 0] = xVals
+    X[:, 1] = yVals1
+    X[:, 2] = yVals2
+    
+    ### minimal plot
+    f, ax1 = plt.subplots(1)
+    
+    '''
+    Using the zorder keyword we plot the first line below the
+    axis, whereas we put the second line above it.
+    Comapare the corresponding zorder values:
+    zorder = 1 and 11 for the two lines
+    and zorder = 10 for the axis, as manually set below.
+    The value of zoder = 10 for the axis should be adjusted
+    for your individual needs.
+    '''
+    
+    ax1.plot(X[:, 0], X[:, 1],
+             alpha = 1.0,
+             color = 'C0',
+             lw = 1.0,
+             label = 'data 1',
+             clip_on = False,
+             zorder = 1)
+
+    ax1.plot(X[:, 0], X[:, 2],
+             alpha = 1.0,
+             color = 'C1',
+             lw = 1.0,
+             label = 'data 2',
+             clip_on = False,
+             zorder = 11)
+
+    ######################################################################################
+    ######################################################################################
+    # manually set the axis zorder here
+    ax1.set_axisbelow(False)
+    for k, spine in ax1.spines.items():  #ax.spines is a dictionary
+        spine.set_zorder(10)
+    ######################################################################################
+    ######################################################################################
+    
+    leg = ax1.legend(handlelength = 1.35, 
+                     scatterpoints = 1,
+                     markerscale = 1.0,
+                     ncol = 1)
+    leg.draw_frame(False)
+        
+    ######################################################################################
+    # set plot range and scale
+    ax1.set_xlim(-0.05, 1.05)              
+    
+    outname += '_' + now
+    f.savefig(os.path.join(OUTDIR, outname) + '.pdf', dpi = 300, transparent = True);
+    plt.show()
+    plt.clf()
+    plt.close()
+
+    
+    
