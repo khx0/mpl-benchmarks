06547318f96ce2eb6300018143872324b4e8cde1
Nikolas Schnellbächer <khx0@posteo.net>
2018-09-01 14:20:55 +0200

    mpl 2.2.3 compatibility

--- mpl_legend_markerwidth/mpl_legend_manually_set_size.py
+++ mpl_legend_markerwidth/mpl_legend_manually_set_size.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-08-12
+# date: 2018-09-01
 # file: mpl_legend_manually_set_size.py
-# tested with python 2.7.15 in conjunction with mpl version 2.2.2
-# tested with python 3.7.0  in conjunction with mpl version 2.2.2
+# tested with python 2.7.15 in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 2.2.3
 ##########################################################################################
 
 import sys
