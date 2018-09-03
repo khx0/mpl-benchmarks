c1a11268c6ba7e084dfa63f45fc79ad23af5b778
Nikolas Schnellbächer <khx0@posteo.net>
2018-09-03 08:49:01 +0200

    mpl 2.2.3 comp.

--- mpl_offset_text_handling/mpl_offset_text_handling_05.py
+++ mpl_offset_text_handling/mpl_offset_text_handling_05.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-08-13
+# date: 2018-09-03
 # file: mpl_offset_text_handling_05.py
-# tested with python 2.7.15 in conjunction with mpl version 2.2.2
-# tested with python 3.7.0  in conjunction with mpl version 2.2.2
+# tested with python 2.7.15 in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 2.2.3
 ##########################################################################################
 
 import time
