398c9436a2ee9be52b71971c7bfa7a1e235dc5c1
Nikolas Schnellbächer <khx0@posteo.net>
2018-09-07 11:06:21 +0200

    mpl 2.2.3 compatibility

--- mpl_standalone_colorbar/mpl_standalone_colorbar.py
+++ mpl_standalone_colorbar/mpl_standalone_colorbar.py
@@ -3,8 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-09-03
+# date: 2018-09-07
 # file: mpl_standalone_colorbar.py
+# tested with python 2.7.15 in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 2.2.3
 ##########################################################################################
 
 import time
