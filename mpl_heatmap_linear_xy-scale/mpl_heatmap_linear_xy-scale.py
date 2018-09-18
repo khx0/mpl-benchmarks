f031e84fedcd1b0a4d6e0a1bf173d3618867a4dc
Nikolas Schnellbächer <khx0@posteo.net>
2018-09-18 09:52:18 +0200

    mpl 2.2.3 compatibility

--- mpl_heatmap_linear_xy-scale/mpl_heatmap_linear_xy-scale.py
+++ mpl_heatmap_linear_xy-scale/mpl_heatmap_linear_xy-scale.py
@@ -5,6 +5,8 @@
 # contact: khx0@posteo.net
 # date: 2018-09-18
 # file: mpl_heatmap_linear_xy-scale.py
+# tested with python 2.7.15 in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 2.2.3
 ##########################################################################################
 
 import sys
