bbc817859623248311bf20c7d4aa478228c72e82
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-11 22:32:36 +0100

    code polish

--- mpl_fonts/available_fonts.py
+++ mpl_fonts/available_fonts.py
@@ -13,15 +13,15 @@ import matplotlib as mpl
 import matplotlib.font_manager
 
 if __name__ == '__main__':
-
+    
     print("mpl.matplotlib_fname():", mpl.matplotlib_fname())
-
+    
     mpl.font_manager.findSystemFonts()
-     
-     
+    
+    
     # print fonts which are available for the given matplotlib installation
     flist = mpl.font_manager.get_fontconfig_fonts()    
-
+    
     for i, fname in enumerate(flist):
         
         print(i, fname)
