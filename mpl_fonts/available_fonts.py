7a62453d3e5a2cc7bc9ef219595e0023f32b29a2
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-11 22:31:54 +0100

    clean up, mpl fonts

--- mpl_fonts/available_fonts.py
+++ mpl_fonts/available_fonts.py
@@ -1,38 +1,27 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-08-30
-# file: mpl_annotate_alignment_minimal.py
+# date: 2019-01-11
+# file: available_fonts.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.0  in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 3.0.2
 ##########################################################################################
 
-import time
-import datetime
-import sys
-import os
-import numpy as np
 import matplotlib as mpl
-from matplotlib import pyplot as plt
-from matplotlib import rc
-from matplotlib.pyplot import legend
-
 import matplotlib.font_manager
 
 if __name__ == '__main__':
 
     print("mpl.matplotlib_fname():", mpl.matplotlib_fname())
-    
+
     mpl.font_manager.findSystemFonts()
-    
-    flist = mpl.font_manager.get_fontconfig_fonts()
-    
-    print(flist)
-    
-    for fname in flist:
-        print(fname)
-    
-    #names = [mpl.font_manager.FontProperties(fname = fname).get_name() for fname in flist]
+     
+     
+    # print fonts which are available for the given matplotlib installation
+    flist = mpl.font_manager.get_fontconfig_fonts()    
+
+    for i, fname in enumerate(flist):
+        
+        print(i, fname)
