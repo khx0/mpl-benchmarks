afe24b3b862a057a355e944bee597dc2109bf3c4
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-10 19:52:47 +0100

    updated python version doc

--- mpl_annotate_alignment/mpl_annotate_alignment.py
+++ mpl_annotate_alignment/mpl_annotate_alignment.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-01-12
+# date: 2019-02-10
 # file: mpl_annotate_alignment.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.0  in conjunction with mpl version 3.0.2
+# tested with python 3.7.2  in conjunction with mpl version 3.0.2
 ##########################################################################################
 
 import os
