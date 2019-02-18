b4e22913bbf370b82ca922972a64d4a8b627753d
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-18 22:10:00 +0100

    added test_ticker.py

---
+++ unit-tests/test_ticker.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2019-02-18
+# file: test_ticker.py
+# tested with python 2.7.15
+# tested with python 3.7.2
+##########################################################################################
+
+import sys
+import platform
+import numpy as np
+import unittest
+
+sys.path.append('../')
+
+from ticker import getLogTicksBase10
+
+class TickerTest(unittest.TestCase):
+    '''
+    Test cases for the ticker module.
+    '''
+    
+    '''
+    def test_linearScale_01(self):
+        
+        xminData = 0.0
+        xmaxData = 2.0
+        paddingFraction = 0.05
+        
+        xmin_reference = -0.1
+        xmax_reference = 2.1
+        
+        xmin, xmax = getLinearAxisPadding(xminData, xmaxData, paddingFraction)
+        
+        self.assertTrue(np.isclose(xmin, xmin_reference))
+        self.assertTrue(np.isclose(xmax, xmax_reference))
+        
+        return None
+
+    def test_linearScale_02(self):
+        
+        xminData = -99.0
+        xmaxData = 53.0
+        paddingFraction = 0.03
+        
+        xmin_reference = -99.0 - 4.56
+        xmax_reference = 53.0 + 4.56
+        
+        xmin, xmax = getLinearAxisPadding(xminData, xmaxData, paddingFraction)
+        
+        self.assertTrue(np.isclose(xmin, xmin_reference))
+        self.assertTrue(np.isclose(xmax, xmax_reference))
+    
+        return None
+    
+    def test_logScale_01(self):
+    
+        xminData = 1.0e-11
+        xmaxData = 1.0e-9
+        paddingFraction = 0.04
+        
+        xmin_reference = 8.317637711026709e-12
+        xmax_reference = 1.202264434617413e-09
+        
+        xmin, xmax = getLogAxisPadding(xminData, xmaxData, paddingFraction)
+        
+        self.assertTrue(np.isclose(xmin, xmin_reference))
+        self.assertTrue(np.isclose(xmax, xmax_reference))
+        
+        return None
+    '''
+
+if __name__ == '__main__':
+    
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("Running ", __file__)
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("Python Interpreter Version =", platform.python_version())
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+    
+    unittest.main()
