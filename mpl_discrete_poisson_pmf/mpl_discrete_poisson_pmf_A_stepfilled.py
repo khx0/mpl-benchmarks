d2273edac4ba0239989ebf80c5956d1cf49f5ed3
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-09 11:07:04 +0100

    mpl 3.0.3 update

--- mpl_discrete_poisson_pmf/mpl_discrete_poisson_pmf_A_stepfilled.py
+++ mpl_discrete_poisson_pmf/mpl_discrete_poisson_pmf_A_stepfilled.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-02-10
+# date: 2019-03-09
 # file: mpl_discrete_poisson_pmf_A_stepfilled.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.2
+# tested with python 3.7.2  in conjunction with mpl version 3.0.3
 ##########################################################################################
 
 import os
