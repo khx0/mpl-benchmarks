eb3ef668dcae470e8bd818597ab18f959e1cabf7
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-11 21:30:29 +0100

    updated to mpl 3.0.3

--- mpl_scatter_histogram/mpl_scatter_histogram.py
+++ mpl_scatter_histogram/mpl_scatter_histogram.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-02-14
+# date: 2019-03-11
 # file: mpl_scatter_histogram.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.2
+# tested with python 3.7.2  in conjunction with mpl version 3.0.3
 ##########################################################################################
 
 import sys
