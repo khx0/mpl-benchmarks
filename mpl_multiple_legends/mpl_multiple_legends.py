ce64f60e523a0210d90e6901f2d28815531ced43
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-11 21:19:34 +0100

    updated to mpl 3.0.3

--- mpl_multiple_legends/mpl_multiple_legends.py
+++ mpl_multiple_legends/mpl_multiple_legends.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-02-12
+# date: 2019-03-11
 # file: mpl_multiple_legends.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.2
+# tested with python 3.7.2  in conjunction with mpl version 3.0.3
 ##########################################################################################
 
 import os
