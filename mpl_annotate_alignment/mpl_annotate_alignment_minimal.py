c5a729c801d8091a7b8d9edf794602eb920a8d04
Nikolas Schnellbächer <khx0@posteo.net>
2019-04-09 10:23:37 +0200

    whitespace clean up

--- mpl_annotate_alignment/mpl_annotate_alignment_minimal.py
+++ mpl_annotate_alignment/mpl_annotate_alignment_minimal.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-03-23
+# date: 2019-04-09
 # file: mpl_annotate_alignment_minimal.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
 # tested with python 3.7.2  in conjunction with mpl version 3.0.3
@@ -28,12 +28,12 @@ OUTDIR = os.path.join(BASEDIR, 'out')
 os.makedirs(OUTDIR, exist_ok = True)
 
 if __name__ == '__main__':
-    
+
     outname = 'mpl_annotate_alignment_minimal'
     outname += '_Python_' + platform.python_version() + \
                '_mpl_' + mpl.__version__
     outname += '_' + now
-    
+
     # create synthetic data
     nVisPoints = 500
     xVals = np.linspace(0.0, 1.0, nVisPoints)
@@ -41,11 +41,11 @@ if __name__ == '__main__':
     X = np.zeros((nVisPoints, 2))
     X[:, 0] = xVals
     X[:, 1] = yVals
-    
+
     # minimal plot
     f, ax1 = plt.subplots(1)
     f.subplots_adjust(right = 0.70)
-    
+
     ax1.plot(X[:, 0], X[:, 1],
              alpha = 1.0,
              color = 'C0',
@@ -53,67 +53,67 @@ if __name__ == '__main__':
              label = 'data',
              clip_on = False,
              zorder = 1)
-    
-    leg = ax1.legend(handlelength = 1.5, 
+
+    leg = ax1.legend(handlelength = 1.5,
                      scatterpoints = 1,
                      markerscale = 1.0,
                      ncol = 1)
     leg.draw_frame(False)
-    
+
     # annotations
     '''
     This example uses relative coordinates for the placement of annotations.
     This is realized by setting xycoords = 'axes fraction'. Then the specified xy
     position is set relative to the axis canvas, where both the x and y position is
-    specified by their fractional value between [0, 1]. 
+    specified by their fractional value between [0, 1].
     Additionally this example demonstrates the use of alignment specifications,
     using the "horizontalalignment" and "verticalalignment" keywords.
     For the horizontal alignment keyword use left, right or center and
     for the vertical alignment keyword use top, center or bottom, respectively.
     '''
-    
+
     ax1.annotate('upper right label',
                  xy = (1.0, 1.03),
                  xycoords = 'axes fraction',
                  horizontalalignment = 'right',
                  zorder = 8)
-    
+
     ax1.annotate('upper left label',
                  xy = (0.0, 1.03),
                  xycoords = 'axes fraction',
                  horizontalalignment = 'left',
                  zorder = 8)
-    
+
     ax1.annotate('center label',
                  xy = (0.5, 0.5),
                  xycoords = 'axes fraction',
                  horizontalalignment = 'center',
                  verticalalignment = 'center',
                  zorder = 8)
-    
+
     ax1.annotate('right margin top label',
                  xy = (1.02, 1.0),
                  xycoords = 'axes fraction',
                  horizontalalignment = 'left',
                  verticalalignment = 'top',
                  zorder = 8)
-    
+
     ax1.annotate('right margin bottom label',
                  xy = (1.02, 0.0),
                  xycoords = 'axes fraction',
                  horizontalalignment = 'left',
                  verticalalignment = 'bottom',
                  zorder = 8)
-    
+
     # set plot range and scale
-    ax1.set_xlim(-0.05, 1.05)              
-    
+    ax1.set_xlim(-0.05, 1.05)
+
     # save plot to file
     f.savefig(os.path.join(OUTDIR, outname) + '.pdf',
               dpi = 300,
               transparent = True)
     plt.show()
-    
+
     # close handles
     plt.cla()
     plt.clf()
