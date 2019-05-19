84c6167e171d4133531fb9c1a9fb461e4cd0ff05
Nikolas Schnellbächer <khx0@posteo.net>
2019-05-19 11:24:41 +0200

    updated to mpl 3.1.0

--- mpl_heatmap_linear_xy-scale/mpl_heatmap_linear_xy-scale.py
+++ mpl_heatmap_linear_xy-scale/mpl_heatmap_linear_xy-scale.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-09
+# date: 2019-05-19
 # file: mpl_heatmap_linear_xy-scale.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.3
+# tested with python 3.7.2  in conjunction with mpl version 3.1.0
 ##########################################################################################
 
 import sys
@@ -50,7 +50,7 @@ def plot_pcolor(X, Y, Z, titlestr, params,
 
     mpl.rc('font', **{'size': 10})
     mpl.rc('legend', **{'fontsize': 8.0})
-    mpl.rc("axes", linewidth = 0.5)
+    mpl.rc('axes', linewidth = 0.5)
 
     # mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Myriad Pro']})
     mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Helvetica']})
