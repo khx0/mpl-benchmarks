d174cd5750b9887bbee138b738d5f6c6845969b9
Nikolas Schnellbächer <khx0@posteo.net>
2019-07-15 23:21:36 +0200

    updated to mpl 3.1.1

--- mpl_offset_text_handling/mpl_offset_text_handling_05.py
+++ mpl_offset_text_handling/mpl_offset_text_handling_05.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-05-30
+# date: 2019-07-15
 # file: mpl_offset_text_handling_05.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.0
+# tested with python 3.7.2 in conjunction with mpl version 3.1.1
 ##########################################################################################
 
 import os
