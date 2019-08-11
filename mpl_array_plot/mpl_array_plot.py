8b473756906092eb67c7475f0b716392b5209955
Nikolas Schnellbächer <khx0@posteo.net>
2019-08-11 20:45:01 +0200

    clean up

--- mpl_array_plot/mpl_array_plot.py
+++ mpl_array_plot/mpl_array_plot.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-07-04
+# date: 2019-08-11
 # file: mpl_array_plot.py
 # tested with python 3.7.2 in conjunction with mpl version 3.1.1
 ##########################################################################################
