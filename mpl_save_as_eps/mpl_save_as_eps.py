3ddc9c03f4450750c8a0297f3cf24925cbcd2d84
Nikolas Schnellbächer <khx0@posteo.net>
2019-08-17 13:20:36 +0200

    updated datetime string formatting

--- mpl_save_as_eps/mpl_save_as_eps.py
+++ mpl_save_as_eps/mpl_save_as_eps.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-07-15
+# date: 2019-08-17
 # file: mpl_save_as_eps.py
 # tested with python 3.7.2 in conjunction with mpl version 3.1.1
 ##########################################################################################
@@ -27,8 +27,7 @@ from matplotlib.pyplot import legend
 
 mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
 
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(str(now.year), str(now.month).zfill(2), str(now.day).zfill(2))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
@@ -138,7 +137,7 @@ def Plot(titlestr, X, outname, outdir, pColors,
     ######################################################################################
     # save to file
     if datestamp:
-        outname += '_' + now
+        outname += '_' + today
     if savePDF: # save to file using pdf backend
         f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True)
     if savePNG:
--- mpl_save_as_eps/mpl_save_as_eps_minimal.py
+++ mpl_save_as_eps/mpl_save_as_eps_minimal.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-07-15
+# date: 2019-08-17
 # file: mpl_save_as_eps_minimal.py
 # tested with python 3.7.2 in conjunction with mpl version 3.1.1
 ##########################################################################################
@@ -19,8 +19,7 @@ from matplotlib.pyplot import legend
 
 mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
 
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(str(now.year), str(now.month).zfill(2), str(now.day).zfill(2))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 OUTDIR = os.path.join(BASEDIR, 'out')
@@ -62,7 +61,7 @@ if __name__ == '__main__':
     ax1.set_xlim(-0.05, 1.05)
 
     # save plot to file
-    outname += '_' + now
+    outname += '_' + today
     f.savefig(os.path.join(OUTDIR, outname) + '.pdf',
               dpi = 300,
               transparent = True)
