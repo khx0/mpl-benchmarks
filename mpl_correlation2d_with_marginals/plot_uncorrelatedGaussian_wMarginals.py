09f60e489276058f567de3eb6e61e6081f5cff1e
Nikolas Schnellbächer <khx0@posteo.net>
2019-12-11 21:29:40 +0100

    updated to mpl 3.1.2

--- mpl_correlation2d_with_marginals/plot_uncorrelatedGaussian_wMarginals.py
+++ mpl_correlation2d_with_marginals/plot_uncorrelatedGaussian_wMarginals.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-08-15
+# date: 2019-12-11
 # file: plot_uncorrelateGaussian_wMarginals.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.1
+# tested with python 3.7.2 in conjunction with mpl version 3.1.2
 ##########################################################################################
 # description: plots the uncorrelated data samples that can be generated using the
 # provided create_samples.py script
