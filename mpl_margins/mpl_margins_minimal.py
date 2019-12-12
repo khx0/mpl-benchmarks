c2a2d33e983809d8cd095906eb65fdca41a6e481
Nikolas Schnellbächer <khx0@posteo.net>
2019-12-13 00:25:25 +0100

    updated to mpl 3.1.2

--- mpl_margins/mpl_margins_minimal.py
+++ mpl_margins/mpl_margins_minimal.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-08-13
+# date: 2019-12-13
 # file: mpl_margins_minimal.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.1
+# tested with python 3.7.2 in conjunction with mpl version 3.1.2
 ##########################################################################################
 
 ##########################################################################################
