d407d95af65282cc737498ba9af18ad37ca7b28f
Nikolas Schnellbächer <khx0@posteo.net>
2019-12-16 15:24:51 +0100

    added unit tests for getPcolorBoxCoordinates

---
+++ unit-tests/test_getPcolorBoxCoordinates.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2019-12-16
+# file: test_getPcolorBoxCoordinates.py
+# tested with python 3.7.2
+##########################################################################################
+
+'''
+Tested with pytest version 5.3.1.
+Invocation:
+cd to the directory containing this script (here ../unit-tests/) and
+then invoke
+$python -m pytest (-v)
+where python is your chosen python interpreter or
+alternatively simply call
+$pytest
+or
+$pytest -v
+using the default python interpreter on your system.
+The -v flag (equal to --verbose) sets the pytest mode to verbose.
+'''
+
+import sys
+import platform
+import numpy as np
+import unittest
+
+sys.path.append('../')
+
+from mplUtils import getPcolorBoxCoordinates
+
+class PColorBoxCoordinatesTest(unittest.TestCase):
+
+    """
+    Test cases for the getPcolorBoxCoordinates function
+    """
+
+    def test_01(self):
+
+        xVals = np.array([0.0])
+        pixelWidth = 1.0
+        res = np.array([-0.5, 0.5])
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals, unitWidth = pixelWidth)
+
+        self.assertTrue(np.array_equal(xBoxCoords, res))
+
+        return None
+
+    def test_02(self):
+
+        xVals = np.array([0.0])
+        pixelWidth = 12.0
+        res = np.array([-6.0, 6.0])
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals, unitWidth = pixelWidth)
+
+        self.assertTrue(np.array_equal(xBoxCoords, res))
+
+        return None
+
+    def test_03(self):
+
+        xVals = np.array([0.0])
+        res = None
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals)
+
+        self.assertTrue(res == xBoxCoords)
+
+        xVals = np.array([9876.1234])
+        res = None
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals)
+
+        self.assertTrue(res == xBoxCoords)
+
+        return None
+
+    def test_04(self):
+
+        xVals = np.array([0.0, 1.0, 2.0])
+        res = np.array([-0.5, 0.5, 1.5, 2.5])
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals)
+
+        self.assertTrue(np.array_equal(xBoxCoords, res))
+        self.assertTrue(res.shape == xBoxCoords.shape)
+        self.assertTrue(len(xVals) + 1 == len(xBoxCoords))
+
+        return None
+
+    def test_04(self):
+
+        xVals = np.array([0.2, 0.4, 0.6])
+        res = np.array([0.1, 0.3, 0.5, 0.7])
+
+        xBoxCoords = getPcolorBoxCoordinates(xVals)
+
+        self.assertTrue(np.array_equal(xBoxCoords, res))
+        self.assertTrue(res.shape == xBoxCoords.shape)
+        self.assertTrue(len(xVals) + 1 == len(xBoxCoords))
+
+        return None
+
+if __name__ == '__main__':
+
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("Running", __file__)
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("Python Interpreter Version =", platform.python_version())
+    print("/////////////////////////////////////////////////////////////////////////////")
+    print("/////////////////////////////////////////////////////////////////////////////")
+
+    unittest.main()
