e156de184a8b82ca3a0e19086d3f8b0f3bc4ca83
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-14 00:01:40 +0100

    updated to mpl 3.1.3

--- mpl_empty/mpl_empty_contour_minimal.py
+++ mpl_empty/mpl_empty_contour_minimal.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-12-11
+# date: 2020-02-14
 # file: mpl_empty_contour_minimal.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.2
+# tested with python 3.7.2 in conjunction with mpl version 3.1.3
 ##########################################################################################
 
 import os
