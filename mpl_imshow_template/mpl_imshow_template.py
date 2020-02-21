1a0d022e3bf423b19d538746b1e70c2c67b72368
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-21 23:07:11 +0100

    updated to mpl 3.1.3

--- mpl_imshow_template/mpl_imshow_template.py
+++ mpl_imshow_template/mpl_imshow_template.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-01-01
+# date: 2020-02-21
 # file: mpl_imshow_template.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.2
+# tested with python 3.7.2 in conjunction with mpl version 3.1.3
 ##########################################################################################
 
 import sys
