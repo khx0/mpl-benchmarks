45356bf7827d078a0f3a6d7ca169ab1a1ed66fa3
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-21 22:52:19 +0100

    updated to mpl 3.1.3

--- mpl_manually_set_axis_zorder/mpl_manually_set_axis_zorder.py
+++ mpl_manually_set_axis_zorder/mpl_manually_set_axis_zorder.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-12-13
+# date: 2020-02-21
 # file: mpl_manually_set_axis_zorder.py
-# tested with python 3.7.2 in conjunction with mpl version 3.1.2
+# tested with python 3.7.2 in conjunction with mpl version 3.1.3
 ##########################################################################################
 
 import os
