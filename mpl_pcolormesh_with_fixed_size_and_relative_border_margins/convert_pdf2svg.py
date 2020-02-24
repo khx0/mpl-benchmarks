6f48fd03e980cd8758561ec45de6bb477788eac5
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-24 21:18:28 +0100

    updated to python 3.7.6

--- mpl_pcolormesh_with_fixed_size_and_relative_border_margins/convert_pdf2svg.py
+++ mpl_pcolormesh_with_fixed_size_and_relative_border_margins/convert_pdf2svg.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-02-20
+# date: 2020-02-24
 # file: convert_pdf2svg.py
-# tested with python 3.7.2
+# tested with python 3.7.6
 ##########################################################################################
 
 import os
