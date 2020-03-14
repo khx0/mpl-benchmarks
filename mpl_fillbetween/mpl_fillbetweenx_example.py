9e803591a3d22d68a5835bc1480bf2403a962e6a
Nikolas Schnellbächer <khx0@posteo.net>
2020-03-14 17:55:02 +0100

    updated to mpl 3.2.0

--- mpl_fillbetween/mpl_fillbetweenx_example.py
+++ mpl_fillbetween/mpl_fillbetweenx_example.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-02-23
+# date: 2020-03-14
 # file: mpl_fillbetweenx_example.py
-# tested with python 3.7.6 in conjunction with mpl version 3.1.3
+# tested with python 3.7.6 in conjunction with mpl version 3.2.0
 ##########################################################################################
 
 import os
@@ -151,7 +151,7 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
 
     ######################################################################################
     # set plot range
-    if (xFormat == None):
+    if xFormat == None:
         pass
     else:
         major_x_ticks = np.arange(xFormat[2], xFormat[3], xFormat[4])
@@ -160,7 +160,7 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
         ax1.set_xticks(minor_x_ticks, minor = True)
         ax1.set_xlim(xFormat[0], xFormat[1])
 
-    if (yFormat == None):
+    if yFormat == None:
         pass
     else:
         major_y_ticks = np.arange(yFormat[2], yFormat[3], yFormat[4])
@@ -211,8 +211,8 @@ if __name__ == '__main__':
     X[:, 0] = xVals
     X[:, 1] = yVals
 
-    xFormat = [0.0, 0.423, 0.0, 0.41, 0.2, 0.1]
-    yFormat = [-4.3, 4.3, -4.0, 4.1, 2.0, 1.0]
+    xFormat = (0.0, 0.423, 0.0, 0.41, 0.2, 0.1)
+    yFormat = (-4.3, 4.3, -4.0, 4.1, 2.0, 1.0)
 
     pColors = ['C0']
 
