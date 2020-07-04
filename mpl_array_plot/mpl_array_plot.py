1476b9e6c2bae1705c490cc7ddb3725ff85d152e
Nikolas Schnellbächer <khx0@posteo.net>
2020-07-04 13:38:44 +0200

    updated to mpl 3.2.2

--- mpl_array_plot/mpl_array_plot.py
+++ mpl_array_plot/mpl_array_plot.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-03-22
+# date: 2020-07-04
 # file: mpl_array_plot.py
-# tested with python 3.7.6 in conjunction with mpl version 3.2.1
+# tested with python 3.7.6 in conjunction with mpl version 3.2.2
 ##########################################################################################
 
 import os
