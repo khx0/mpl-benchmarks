6a390d74e1d72c330c486d9f906d654be1463ca4
Nikolas Schnellbächer <khx0@posteo.net>
2020-11-09 08:31:58 +0100

    updated mpl_array_plot.py

--- mpl_array_plot/mpl_array_plot.py
+++ mpl_array_plot/mpl_array_plot.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-10-04
+# date: 2020-11-09
 # file: mpl_array_plot.py
 # tested with python 3.7.6 in conjunction with mpl version 3.3.2
 ##########################################################################################
@@ -18,7 +18,6 @@ import matplotlib.pyplot as plt
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
-RAWDIR = os.path.join(BASEDIR, 'raw')
 OUTDIR = os.path.join(BASEDIR, 'out')
 
 os.makedirs(OUTDIR, exist_ok = True)
@@ -51,7 +50,7 @@ def plot_patch_array(X, nrows, ncols, outname, outdir, cmap,
     if datestamp:
         outname += '_' + today         
     if savePNG:
-        f.savefig(os.path.join(OUTDIR, outname) + '.png',
+        f.savefig(os.path.join(outdir, outname) + '.png',
                   dpi = 600,
                   transparent = transparent)
 
@@ -64,23 +63,23 @@ def plot_patch_array(X, nrows, ncols, outname, outdir, cmap,
 if __name__ == '__main__':
 
     # (pseudo) random number handling
-    randomSeed = 123456789
+    random_seed = 123456789
     # fix random state for reproducibility
-    np.random.seed(randomSeed)
+    np.random.seed(random_seed)
 
     # create synthetic data
-    patchShape = (5, 5)
+    patch_shape = (5, 5)
 
-    nPatches = 25
+    n_patches = 25
     patches = []
-    for idx in range(nPatches):
-        tmp = np.random.rand(patchShape[0], patchShape[1])
+    for idx in range(n_patches):
+        tmp = np.random.rand(patch_shape[0], patch_shape[1])
         # print(idx, tmp.shape)
         patches.append(tmp)
     print("len(patches) =", len(patches))
 
     # call plotting function
-    outname = 'array_plot_seed_' + str(randomSeed).zfill(2)
+    outname = 'array_plot_seed_' + str(random_seed).zfill(2)
 
     plot_patch_array(patches, 5, 5, outname, OUTDIR, cmap = 'gray')
     plot_patch_array(patches, 5, 5, outname, OUTDIR, cmap = 'gray', transparent = False)
