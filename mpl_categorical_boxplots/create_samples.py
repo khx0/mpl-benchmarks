af852c377a0ac31bdcd39185e868c3df97d9cff8
Nikolas Schnellbächer <khx0@posteo.net>
2020-11-10 09:10:08 +0100

    improved variable naming convention

--- mpl_categorical_boxplots/create_samples.py
+++ mpl_categorical_boxplots/create_samples.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-10-12
+# date: 2020-11-10
 # file: create_samples.py
-# tested with python 3.7.6 and numpy 1.19.2
+# tested with python 3.7.6 and numpy 1.19.4
 ##########################################################################################
 # description:
 # Creates 1d normal random samples and saves them to the disk as *.dat and *.npy files.
@@ -28,35 +28,35 @@ CREATE_TXT = False
 
 if __name__ == '__main__':
 
-    print("running on python", platform.python_version())
+    print("running python", platform.python_version())
     print("using np.__version__ =", np.__version__)
 
     # set parameters
-    nSamples = 200
+    n_samples = 200
 
     # fixing random state for reproducibility of results
-    seedValue = 987654321
-    np.random.seed(seedValue)
+    seed_value = 987654321
+    np.random.seed(seed_value)
 
     # create samples
-    sample_A = np.random.normal(loc = 2.0, scale = 1.0, size = nSamples)
+    sample_A = np.random.normal(loc = 2.0, scale = 1.0, size = n_samples)
     print("sample_A.shape =", sample_A.shape)
 
     # save samples to file
-    outname = f'normal_samples_np_seed_{seedValue}'
+    outname = f'normal_samples_np_seed_{seed_value}'
     np.save(os.path.join(RAWDIR, outname), sample_A)
     if CREATE_TXT:
         np.savetxt(os.path.join(RAWDIR, outname + '.txt'), sample_A, fmt = '%.8f')
 
-    sample_B = np.random.normal(loc = 1.4, scale = 0.45, size = nSamples)
+    sample_B = np.random.normal(loc = 1.4, scale = 0.45, size = n_samples)
     print("sample_B.shape =", sample_B.shape)
 
-    data = np.zeros((nSamples, 2))
+    data = np.zeros((n_samples, 2))
     data[:, 0] = sample_A
     data[:, 1] = sample_B
 
     # save samples to file
-    outname = f'normal_samples_AB_np_seed_{seedValue}'.format(seedValue)
+    outname = f'normal_samples_AB_np_seed_{seed_value}'
     np.save(os.path.join(RAWDIR, outname), data)
     if CREATE_TXT:
         np.savetxt(os.path.join(RAWDIR, outname + '.txt'), data, fmt = '%.8f')
