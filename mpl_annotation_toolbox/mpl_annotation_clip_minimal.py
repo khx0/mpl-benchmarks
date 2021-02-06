ae67b9f076634d27ab4e851657df74526e9d0115
Nikolas Schnellbächer <khx0@posteo.net>
2021-02-06 16:49:32 +0100

    updated to mpl 3.3.4

--- mpl_annotation_toolbox/mpl_annotation_clip_minimal.py
+++ mpl_annotation_toolbox/mpl_annotation_clip_minimal.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-11-14
+# date: 2021-02-06
 # file: mpl_annotation_clip_minimal.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.3
+# tested with python 3.7.6 in conjunction with mpl version 3.3.4
 ##########################################################################################
 
 import os
