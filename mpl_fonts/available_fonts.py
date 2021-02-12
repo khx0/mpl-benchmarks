e58a7b5f2dd564a98be16b765e6762f1f1b9e25c
Nikolas Schnellbächer <khx0@posteo.net>
2021-02-12 23:01:19 +0100

    updated string formatting and tested with mpl 3.3.4

--- mpl_fonts/available_fonts.py
+++ mpl_fonts/available_fonts.py
@@ -3,14 +3,21 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-10-31
+# date: 2021-02-12
 # file: available_fonts.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.2
+# tested with python 3.7.6 in conjunction with mpl version 3.3.4
 ##########################################################################################
 
 import platform
 import matplotlib as mpl
 import matplotlib.font_manager
+import numpy as np
+
+def number_of_digits(n: int) -> int:
+    '''
+    Returns the number of digits of a given integer n.
+    '''
+    return int(np.floor(np.log10(np.abs(n)) + 1))
 
 if __name__ == '__main__':
 
@@ -25,6 +32,10 @@ if __name__ == '__main__':
     # print fonts which are available for the given matplotlib installation
     flist = mpl.font_manager.get_fontconfig_fonts()
 
+    n_digits = number_of_digits(len(flist))
+
     for i, fname in enumerate(flist):
 
-        print("font #", i, "-->", fname)
+        print("font #", str(i + 1).zfill(n_digits), "-->", fname)
+
+    print(f'{len(flist)} fonts detected')
