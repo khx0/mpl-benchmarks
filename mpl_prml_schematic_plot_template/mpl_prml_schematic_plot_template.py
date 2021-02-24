082776f8a288e2882341fc2d8b709de89bca4c8d
Nikolas Schnellbächer <khx0@posteo.net>
2021-02-24 22:55:53 +0100

    updated to mpl 3.3.4

--- mpl_prml_schematic_plot_template/mpl_prml_schematic_plot_template.py
+++ mpl_prml_schematic_plot_template/mpl_prml_schematic_plot_template.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-11-13
+# date: 2021-02-24
 # file: mpl_prml_schematic_plot_template.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.3
+# tested with python 3.7.6 in conjunction with mpl version 3.3.4
 ##########################################################################################
 
 import os
