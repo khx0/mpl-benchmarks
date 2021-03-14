964b86e6b62fc0a4b93c41bba0b145c2b459fb92
Nikolas Schnellbächer <khx0@posteo.net>
2021-03-14 12:17:25 +0100

    side margin control added

---
+++ mpl_imshow_height_ratio_no_gaps/mpl_imshow_AB_panel_no_gaps_side_margin_control.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2021-03-14
+# file: mpl_imshow_AB_panel_side_margin_control.py
+# tested with python 3.7.6 in conjunction with mpl version 3.3.4
+##########################################################################################
+
+import os
+import platform
+import datetime
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
+
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+OUTDIR = os.path.join(BASEDIR, 'out')
+
+os.makedirs(OUTDIR, exist_ok = True)
+
+if __name__ == '__main__':
+
+    outname = 'mpl_imshow_AB_panel_no_gaps_side_margin_control'
+    outname += '_Python_' + platform.python_version() + \
+               '_mpl_' + mpl.__version__
+    outname += '_' + today
+
+    dpi = 100
+
+    n_rows = 1
+    n_cols = 2
+
+    matrix_size = 512
+
+    data = [np.random.rand(matrix_size, matrix_size), 
+            np.random.rand(matrix_size, matrix_size)]
+
+    cmaps = ['viridis', 'gray']
+
+    heights = [matrix_size]
+    widths = [matrix_size, matrix_size]
+
+    ################################################################################################
+    # figure size and margin control
+    # Step 1 - Set desired figure width by user (in inches).
+    # Here we use this widht as a starting point, from which all other measures are determind.
+    # This could equally be done using the height as pivotal starting point.
+    fig_width_img = 6.0
+
+    # Step 2 - Derive image figure width (assuming no white space margin)
+    # from image content size ratios.
+    fig_height_img = fig_width_img * sum(heights) / sum(widths)
+
+    # Step 3 - Set top margin in percent of the already set figure height.
+    top_height_frac = 0.1    # fraction
+    bottom_height_frac = 0.1 # fraction
+    left_width_frac = 0.02   # fraction
+    right_width_frac = 0.02  # fraction
+
+    top_margin = top_height_frac * fig_height_img       # in inches
+    bottom_margin = bottom_height_frac * fig_height_img # in inches
+    left_margin = left_width_frac * fig_width_img       # in inches
+    right_margin = right_width_frac * fig_width_img     # in inches
+
+    fig_width = np.round(fig_width_img + left_margin + right_margin , 2)
+    fig_height = np.round(fig_height_img + top_margin + bottom_margin, 2)
+
+    # Step 4 - Compute figure margins from set lenghts.
+    bottom_frac = top_margin / fig_height
+    top_frac = 1.0 - bottom_margin / fig_height
+    left_frac = left_margin / fig_width
+    right_frac = 1.0 - right_margin / fig_width
+    ################################################################################################
+
+    f, axs = plt.subplots(nrows = n_rows, ncols = n_cols,
+        figsize = (fig_width, fig_height),
+        gridspec_kw = {'height_ratios': heights},
+        dpi = dpi
+    )
+
+    for j in range(n_cols):
+        axs[j].imshow(data[j], cmap = cmaps[j])
+        axs[j].axis('off')
+
+    plt.subplots_adjust(
+        wspace = 0.0, hspace = 0.0,
+        left = left_frac, right = right_frac,
+        bottom = bottom_frac, top = top_frac
+    )
+
+    # save plot to file
+    f.savefig(os.path.join(OUTDIR, outname) + '.png',
+              transparent = True)
+    plt.show()
+
+    # close handles
+    plt.cla()
+    plt.clf()
+    plt.close()
