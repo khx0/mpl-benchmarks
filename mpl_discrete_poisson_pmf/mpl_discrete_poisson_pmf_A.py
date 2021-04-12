e270ad0bcbd2973f04dee2b9d18e0fabc070e276
Nikolas Schnellbächer <khx0@posteo.net>
2021-04-12 11:59:21 +0200

    updated to mpl 3.4.1

--- mpl_discrete_poisson_pmf/mpl_discrete_poisson_pmf_A.py
+++ mpl_discrete_poisson_pmf/mpl_discrete_poisson_pmf_A.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-02-08
+# date: 2021-04-12
 # file: mpl_discrete_poisson_pmf_A.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.4
+# tested with python 3.7.6 in conjunction with mpl version 3.4.1
 ##########################################################################################
 
 import os
