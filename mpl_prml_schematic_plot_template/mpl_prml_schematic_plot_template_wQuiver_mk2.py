97b234294baaeb6d6b26c34c2ffd0acd11ff8999
Nikolas Schnellbächer <khx0@posteo.net>
2021-04-19 21:44:07 +0200

    updated to mpl 3.4.1

--- mpl_prml_schematic_plot_template/mpl_prml_schematic_plot_template_wQuiver_mk2.py
+++ mpl_prml_schematic_plot_template/mpl_prml_schematic_plot_template_wQuiver_mk2.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-02-25
+# date: 2021-04-19
 # file: mpl_prml_schematic_plot_template_wQuiver_mk2.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.4
+# tested with python 3.7.6 in conjunction with mpl version 3.4.1
 ##########################################################################################
 
 import os
