cab6276202ea954e45ccd4165c0cd1f031cd5ad7
Nikolas Schnellbächer <khx0@posteo.net>
2021-04-23 22:01:13 +0200

    updated to mpl 3.4.1

--- mpl_pcolormesh_with_fixed_size_and_relative_border_margins/mpl_pcolormesh_with_fixed_size_and_relative_border_margins.py
+++ mpl_pcolormesh_with_fixed_size_and_relative_border_margins/mpl_pcolormesh_with_fixed_size_and_relative_border_margins.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-02-25
+# date: 2021-04-23
 # file: mpl_pcolormesh_with_fixed_size_and_relative_border_margins.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.4
+# tested with python 3.7.6 in conjunction with mpl version 3.4.1
 ##########################################################################################
 
 import sys
