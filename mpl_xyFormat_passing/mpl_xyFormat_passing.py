1497722e10d572954fc078ef7abbac40e0adf947
Nikolas Schnellbächer <khx0@posteo.net>
2021-04-26 10:41:23 +0200

    updated to mpl 3.4.1

--- mpl_xyFormat_passing/mpl_xyFormat_passing.py
+++ mpl_xyFormat_passing/mpl_xyFormat_passing.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-02-22
+# date: 2021-04-26
 # file: mpl_xyFormat_passing.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.4
+# tested with python 3.7.6 in conjunction with mpl version 3.4.1
 ##########################################################################################
 
 ##########################################################################################
