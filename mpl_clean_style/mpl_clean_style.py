63125d61fb75b3341565775d10a6f4ce9b61421c
Nikolas Schnellbächer <khx0@posteo.net>
2021-05-10 16:12:15 +0200

    updated to mpl 3.4.2

--- mpl_clean_style/mpl_clean_style.py
+++ mpl_clean_style/mpl_clean_style.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-04-11
+# date: 2021-05-10
 # file: mpl_clean_style.py
-# tested with python 3.7.6 in conjunction with mpl version 3.4.1
+# tested with python 3.7.6 in conjunction with mpl version 3.4.2
 ##########################################################################################
 
 import os
