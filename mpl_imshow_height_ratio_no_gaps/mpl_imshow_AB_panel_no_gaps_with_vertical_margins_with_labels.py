4598765c2c19c7ee181e965aa37c487a21f225f6
Nikolas Schnellbächer <khx0@posteo.net>
2021-05-14 11:17:51 +0200

    updated to mpl 3.4.2

--- mpl_imshow_height_ratio_no_gaps/mpl_imshow_AB_panel_no_gaps_with_vertical_margins_with_labels.py
+++ mpl_imshow_height_ratio_no_gaps/mpl_imshow_AB_panel_no_gaps_with_vertical_margins_with_labels.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-04-20
+# date: 2021-05-14
 # file: mpl_imshow_AB_panel_no_gaps_with_vertical_margins_with_labels.py
-# tested with python 3.7.6 in conjunction with mpl version 3.4.1
+# tested with python 3.7.6 in conjunction with mpl version 3.4.2
 ##########################################################################################
 
 import os
