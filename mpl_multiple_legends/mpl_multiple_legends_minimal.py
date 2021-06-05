32a172d0a786ae9ae088120be9250aa5e1c44c59
Nikolas Schnellbächer <khx0@posteo.net>
2021-06-05 11:29:14 +0200

    updated to mpl 3.4.2

--- mpl_multiple_legends/mpl_multiple_legends_minimal.py
+++ mpl_multiple_legends/mpl_multiple_legends_minimal.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2021-04-23
+# date: 2021-06-05
 # file: mpl_multiple_legends_minimal.py
-# tested with python 3.7.6 in conjunction with mpl version 3.4.1
+# tested with python 3.7.6 in conjunction with mpl version 3.4.2
 ##########################################################################################
 
 import os
